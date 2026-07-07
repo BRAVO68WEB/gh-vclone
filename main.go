c367c53ecd9f453aa22062fc95256e0cb860dc17
Nuthan <nuthan@b68.dev>
2026-07-07 13:19:42 +0000

    feat: added pagination

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/cli/go-gh/v2"
+	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 	"github.com/spf13/cobra"
 )
@@ -43,6 +44,101 @@ func prepareQuery(opts *SearchOptions) string {
 	return fmt.Sprintf("org:%s", opts.Query)
 }
 
+func pagingRepositories(repos []Repository, pageSize, page int) []Repository {
+	if pageSize <= 0 {
+		pageSize = 5
+	}
+
+	if len(repos) == 0 {
+		return nil
+	}
+
+	start := page * pageSize
+	if start >= len(repos) {
+		return nil
+	}
+
+	end := start + pageSize
+	if end > len(repos) {
+		end = len(repos)
+	}
+
+	return repos[start:end]
+}
+
+func renderRepo(repoList []Repository, uname string) {
+	currentPage := 0
+	pageSize := 10
+	totalPages := len(repoList) / pageSize
+	if len(repoList)%pageSize != 0 {
+		totalPages++
+	}
+	app := tview.NewApplication()
+
+	list := tview.NewList()
+
+	// Add a footer to the list
+	list.SetBorder(true).SetTitle("Press Enter to select a repository")
+
+	layout := tview.NewFlex().SetDirection(tview.FlexRow).
+		AddItem(list, 0, 1, true)
+
+	render := func(page int) {
+		list.Clear()
+		Repos := pagingRepositories(repoList, pageSize, page)
+		for _, repo := range Repos {
+			list.AddItem(repo.Name, repo.Description, 0, nil)
+		}
+		layout.SetBorder(true).SetTitle(fmt.Sprintf("Page %d/%d", page+1, totalPages))
+	}
+	render(currentPage)
+	userSelected := false
+	userSelectedRepo := ""
+
+	// Wait for user to select a repository and close the app and return the selected repository
+	list.SetSelectedFunc(func(index int, mainText string, secondaryText string, shortcut rune) {
+		app.Stop()
+		userSelected = true
+		userSelectedRepo = mainText
+	})
+	list.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		switch event.Key() {
+		case tcell.KeyLeft:
+			if currentPage > 0 {
+				currentPage--
+				render(currentPage)
+			}
+			return nil
+		case tcell.KeyRight:
+			if currentPage < totalPages-1 {
+				currentPage++
+				render(currentPage)
+			}
+			return nil
+		}
+		return event
+	})
+
+	if err := app.SetRoot(layout, true).Run(); err != nil {
+		panic(err)
+	}
+
+	if userSelected {
+		fmt.Printf("📚 Repository Selected: %s\n", userSelectedRepo)
+
+		gh.Exec("repo", "clone", uname+"/"+userSelectedRepo)
+
+		fmt.Printf("📂 Repository %s cloned\n", userSelectedRepo)
+
+		fmt.Printf("☕ Happy coding!\n")
+	}
+
+	// If the user didn't select a repository, exit the program
+	if !userSelected {
+		fmt.Printf("🤷‍♀️ no repository selected\n")
+	}
+}
+
 func runSearch(opts *SearchOptions) error {
 	args := []string{"api", "users/" + opts.Query, "--jq", ".type"}
 
@@ -74,45 +170,7 @@ func runSearch(opts *SearchOptions) error {
 			return err
 		}
 
-		app := tview.NewApplication()
-
-		list := tview.NewList()
-
-		// Add a footer to the list
-		list.SetBorder(true).SetTitle("Press Enter to select a repository")
-
-		for _, repo := range repoList {
-			list.AddItem(repo.Name, repo.Description, '*', nil)
-		}
-
-		userSelected := false
-		userSelectedRepo := ""
-
-		// Wait for user to select a repository and close the app and return the selected repository
-		list.SetSelectedFunc(func(index int, mainText string, secondaryText string, shortcut rune) {
-			app.Stop()
-			userSelected = true
-			userSelectedRepo = mainText
-		})
-
-		if err := app.SetRoot(list, true).Run(); err != nil {
-			panic(err)
-		}
-
-		if userSelected {
-			fmt.Printf("📚 Repository Selected: %s\n", userSelectedRepo)
-
-			gh.Exec("repo", "clone", opts.Query+"/"+userSelectedRepo)
-
-			fmt.Printf("📂 Repository %s cloned\n", userSelectedRepo)
-
-			fmt.Printf("☕ Happy coding!\n")
-		}
-
-		// If the user didn't select a repository, exit the program
-		if !userSelected {
-			fmt.Printf("🤷‍♀️ no repository selected")
-		}
+		renderRepo(repoList, opts.Query)
 
 	} else if accTypeString == "User\n" {
 		fmt.Printf("🧛‍♂️ User: %s\n", opts.Query)
@@ -133,45 +191,7 @@ func runSearch(opts *SearchOptions) error {
 			return err
 		}
 
-		app := tview.NewApplication()
-
-		list := tview.NewList()
-
-		// Add a footer to the list
-		list.SetBorder(true).SetTitle("Press Enter to select a repository")
-
-		for _, repo := range repoList {
-			list.AddItem(repo.Name, repo.Description, '*', nil)
-		}
-
-		userSelected := false
-		userSelectedRepo := ""
-
-		// Wait for user to select a repository and close the app and return the selected repository
-		list.SetSelectedFunc(func(index int, mainText string, secondaryText string, shortcut rune) {
-			app.Stop()
-			userSelected = true
-			userSelectedRepo = mainText
-		})
-
-		if err := app.SetRoot(list, true).Run(); err != nil {
-			panic(err)
-		}
-
-		if userSelected {
-			fmt.Printf("📚 Repository Selected: %s\n", userSelectedRepo)
-
-			gh.Exec("repo", "clone", opts.Query+"/"+userSelectedRepo)
-
-			fmt.Printf("📂 Repository %s cloned\n", userSelectedRepo)
-
-			fmt.Printf("☕ Happy coding!\n")
-		}
-
-		// If the user didn't select a repository, exit the program
-		if !userSelected {
-			fmt.Printf("🤷‍♀️ no repository selected")
-		}
+		renderRepo(repoList, opts.Query)
 	} else {
 		fmt.Printf("🤷‍♀️ %s is not a valid user or organization", opts.Query)
 	}
